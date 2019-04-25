a76c1de101d0b24018aa7deca35d6f3b15c87050
Shelley Arnold <shelle.kay@gmail.com>
2019-04-25 08:36:21 -0500

    added comments detailing functions

--- src/scripts/postData.js
+++ src/scripts/postData.js
@@ -1,3 +1,4 @@
+//POST Function
 function postNewData(url, data) {
     console.log("postData")
     return fetch(url, {
@@ -14,25 +15,28 @@ function postNewData(url, data) {
     })
 }
 
+//Gloabl functions were placed in each js file
 gloablItineraryButton = () => {
+    //declare vaiables before if statement
     let parkInfo = document.querySelector("#parkItinerary").innerText;
     let concertInfo = document.querySelector("#concertItinerary").innerText;
     let resturantInfo = document.querySelector("#restaurantItinerary").innerText;
     let meetupInfo = document.querySelector("#meetupItinerary").innerText;
+    //if statment to check if all div containers are filled
     if (parkInfo !== "" && concertInfo !== "" && resturantInfo !== "" && meetupInfo !== "") {
-      let itineraryButtonContainer = document.querySelector(
-        "#saveItineraryContainer"
-      );
+      //select element to add button
+        let itineraryButtonContainer = document.querySelector( "#saveItineraryContainer");
       itineraryButtonContainer.innerHTML =
         "<button type=\"button\" id=\"saveToJSONButton\">Save Itinerary</button>";
-      document.querySelector("#saveToJSONButton").addEventListener(
-        "click", () => {
-        postNewData("http://localhost:8088/Itinerary", {
-          park: parkInfo,
-          concert: concertInfo,
-          resturant: resturantInfo,
-          meetup: meetupInfo
-        })
-      });
+      //add even listiner to button AND add anonymous function to call button
+        document.querySelector("#saveToJSONButton").addEventListener("click", () => {
+            postNewData("http://localhost:8088/Itinerary", {
+              park: parkInfo,
+              concert: concertInfo,
+              resturant: resturantInfo,
+              meetup: meetupInfo
+            });
+          }
+        );
     }
   };
